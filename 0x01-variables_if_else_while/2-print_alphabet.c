1cd41c09908fa0c1401840d674b2a2c55202f623
lusalman <salmanolans@gmail.com>
2022-12-09 15:33:11 -0800

    The last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - Prints alpha
+ *
+ * Return : Always o (Success)
+ */
+int main(void)
+{
+	char l;
+
+	for (lc = "a"; lc <= "z"; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n')
+	return (0);
+}
