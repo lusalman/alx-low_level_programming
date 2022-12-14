c667b22ecbf80271a83d835f87cb81182370a088
lusalman <salmanolans@gmail.com>
2022-12-14 02:54:36 -0800

    summation of multiples

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,15 @@
 #include<stdio.h>
 
 /**
- * main - lists all natural numbers 1024(excluded) that are multiples of 3 and 5
+ * main - lists all natural numbers 1024(excluded)
+ *		that are multiples of 3 and 5
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int i,sum = 0;
-	
+	int i, sum = 0;
+
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
