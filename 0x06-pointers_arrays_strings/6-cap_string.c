1af45db42b053e88ad604e1915562eb2b712006b
lusalman <salmanolans@gmail.com>
2022-12-22 13:21:59 -0800

    leet

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,30 +8,30 @@
  */
 char *cap_string(char *str)
 {
-		int index = 0;
+		int i = 0;
 
-		while (str[index])
+		while (str[i])
 		{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-		index++;
+		while (!(str[i >= 'a' && str[i] <= 'z'))
+		i++;
 
-		if (str[index - 1] == ' ' ||
-		str[index - 1] == '\t' ||
-		str[index - 1] == '\n' ||
-		str[index - 1] == ',' ||
-		str[index - 1] == ';' ||
-		str[index - 1] == '.' ||
-		str[index - 1] == '!' ||
-		str[index - 1] == '?' ||
-		str[index - 1] == '"' ||
-		str[index - 1] == '(' ||
-		str[index - 1] == ')' ||
-		str[index - 1] == '{' ||
-		str[index - 1] == '}' ||
+		if (str[i - 1] == ' ' ||
+		str[i - 1] == '\t' ||
+		str[i - 1] == '\n' ||
+		str[i - 1] == ',' ||
+		str[i - 1] == ';' ||
+		str[i - 1] == '.' ||
+		str[i - 1] == '!' ||
+		str[i - 1] == '?' ||
+		str[i - 1] == '"' ||
+		str[i - 1] == '(' ||
+		str[i - 1] == ')' ||
+		str[i - 1] == '{' ||
+		str[i - 1] == '}' ||
 		index == 0)
-		str[index] -= 32;
+		str[i] -= 32;
 
-		index++;
+		i++;
 		}
 
 		return (str);
